b9046c45538830176942c7cfe53b24438b2762f4
Ronny Morel <ronnymorel@Ronnys-MacBook-Pro.local>
2023-10-23 22:55:46 +0200

    added a sync excercise

--- src/lectures/exercise/sync/sync.go
+++ src/lectures/exercise/sync/sync.go
@@ -19,7 +19,66 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"unicode"
 )
 
-func main() {}
+type Count struct {
+	count int
+	sync.Mutex
+}
+
+func getWords(line string) []string {
+	return strings.Split(line, " ")
+}
+
+func countLetters(word string) int {
+	letter := 0
+	for _, ch := range word {
+		if unicode.IsLetter(ch) {
+			letter += 1
+		}
+	}
+	return letter
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	totalLetters := Count{}
+	var wg sync.WaitGroup
+
+	for {
+		if scanner.Scan() {
+			line := scanner.Text()
+			words := getWords(line)
+
+			for _, word := range words {
+				wordCopy := word
+				wg.Add(1)
+				go func() {
+					totalLetters.Lock()
+					defer totalLetters.Unlock()
+					defer wg.Done()
+					sum := countLetters(wordCopy)
+					totalLetters.count += sum
+				}()
+			}
+		} else {
+			break
+		}
+
+	}
+
+	wg.Wait()
+	totalLetters.Lock()
+	sum := totalLetters.count
+	totalLetters.Unlock()
+
+	fmt.Println("total letters =", sum)
+
+}
